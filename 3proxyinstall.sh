eb4e6c10ee6c796df80104de21857665962b0edf
Snoy <thestar.illuminate.92@gmail.com>
2017-06-04 15:50:35 +0700

    Add installer, config & service

---
+++ 3proxyinstall.sh
@@ -0,0 +1,19 @@
+version=0.8.9
+apt-get update && apt-get -y upgrade
+apt-get install gcc make git -y
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
+tar xzf 3proxy-${version}.tar.gz
+cd 3proxy-${version}
+make -f Makefile.Linux
+cd src
+mkdir /etc/3proxy/
+mv 3proxy /etc/3proxy/
+cd /etc/3proxy/
+wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+chmod 600 /etc/3proxy/3proxy.cfg
+wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/.proxyauth
+chmod 600 /etc/3proxy/.proxyauth
+cd /etc/init.d/
+wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+chmod  +x /etc/init.d/3proxy
+update-rc.d 3proxy defaults
---
+++ 3proxyuninstall.sh
@@ -0,0 +1,10 @@
+/etc/init.d/3proxy stop
+rm -rf /etc/3proxy
+rm /etc/rc0.d/*proxy
+rm /etc/rc1.d/*proxy
+rm /etc/rc6.d/*proxy
+rm /etc/rc2.d/*proxy
+rm /etc/rc3.d/*proxy
+rm /etc/rc4.d/*proxy
+rm /etc/rc5.d/*proxy
+rm /etc/init.d/3proxy
