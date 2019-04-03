7b478504ca0280928ce823c1555e739f97bfb530
Snoy <thestar.illuminate.92@gmail.com>
2019-04-03 13:55:45 +0700

    Update proxy version

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,7 +1,7 @@
-version=0.8.9
+version=0.8.12
 apt-get update && apt-get -y upgrade
 apt-get install gcc make git -y
-wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/SnoyIatK/3proxy/raw/master/archive/3proxy-${version}.tar.gz
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
 make -f Makefile.Linux
