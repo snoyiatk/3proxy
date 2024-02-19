2d8062e272839d6d76b6e46d36a1f8388308ae75
omelsergey <omels@yandex.ru>
2024-02-19 12:01:11 +0300

    Update 3proxyinstall.sh
    add new version 0.9.3

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,7 +1,7 @@
-version=0.8.13
+version=0.9.3
 apt-get update && apt-get -y upgrade
 apt-get install gcc make git -y
-wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/3proxy/3proxy/releases/download/${version}/3proxy-${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
 make -f Makefile.Linux
